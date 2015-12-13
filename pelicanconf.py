c960fbab7656d9bbb6e87fb2fe5c9c4f53c492a0
André Luiz <contato@xdvl.info>
2015-12-12 22:15:41 -0200

    Move o CNAME pro lugar correto para que ele seja colocado na branch master quando o travis terminar a build

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,13 +40,13 @@ DEFAULT_PAGINATION = 10
 # Extras
 STATIC_PATHS = [
     'images',
-    # 'extras/CNAME',
+    'extras/CNAME',
     'extras/robots.txt',
     'extras/README',
 ]
 
 EXTRA_PATH_METADATA = {
-    # 'extras/CNAME': {'path': 'CNAME'},
+    'extras/CNAME': {'path': 'CNAME'},
     'extras/robots.txt': {'path': 'robots.txt'},
     'extras/README': {'path': 'README'}
 }
