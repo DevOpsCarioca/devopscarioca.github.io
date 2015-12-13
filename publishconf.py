0d62731051968e684cee8200d5786529dd238f9b
André Luiz <contato@xdvl.info>
2015-12-12 22:27:26 -0200

    Não usa https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.devopscarioca.com.br'
+SITEURL = 'http://www.devopscarioca.com.br'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
